20d6f39448c398dd2a6afd9129dc83dc0851b4ca
yunshwang <hwang.yunson@joongang.co.kr>
2016-06-16 10:11:53 +0900

    대쉬보드 추가

---
+++ client/dashboard.js
@@ -0,0 +1,16 @@
+Template.dashboard.onCreated(function(){
+    this.subscribe('feelingLucky');
+});
+Template.dashboard.helpers({
+    'luckyPage':function(){
+        var post = Posts.findOne();
+        return post && post.pageId;
+    },
+    'pages':function(){
+        var result = [];
+        for(var i in Meteor.user().profile.followings){
+            result.push({pageId:i});
+        }
+        return result;
+    }
+});
--- server/publish.js
+++ server/publish.js
@@ -2,3 +2,9 @@ Meteor.publish('getPage',function(pageId){
     return Posts.find({pageId:pageId});
     //return Posts.find();
 });
+Meteor.publish('feelingLucky',function(){
+    return Posts.find({},{
+        skip:Math.random()*Posts.find().count(),
+        limit:1
+    });
+});
